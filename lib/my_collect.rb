ee5e8f41a788ad7c59f9507c2ec2aa3c03267f64
Damon R <dmarcr1997@gmail.com>
2020-01-24 22:58:34 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,11 @@
 def my_collect(array)
   if block_given? 
-    
-  end
+    i = 0
+    collection=[]
+    while i < array.length
+      collection << yield array[i]
+    end
+    collection
+  else
+    puts"No block given."
 end
